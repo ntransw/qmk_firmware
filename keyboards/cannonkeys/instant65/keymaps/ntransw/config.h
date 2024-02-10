6548811fd302abeeb4d4d23e34f793d7626617d2
Nathan Tran <ntranswe@gmail.com>
2024-02-09 19:32:41 -0800

    Add right click and increase speed

--- keyboards/cannonkeys/instant65/keymaps/ntransw/config.h
+++ keyboards/cannonkeys/instant65/keymaps/ntransw/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MK_KINETIC_SPEED
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_INITIAL_SPEED 40
+#define MOUSEKEY_INITIAL_SPEED 2000
 #define MOUSEKEY_INTERVAL 7
 
 /*
